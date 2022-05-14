5a15f3c934621e62a6e56e9277ba622cce607421
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-05-13 22:05:46 -0300

    'scss'

---
+++ board/src/pages/index.tsx
@@ -0,0 +1,16 @@
+
+import Head from 'next/head';
+import styles from '../styles/styles.module.scss';
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+        <title>Board - Organizando suas tarefas.</title>
+    </Head>
+    <div>
+      <h1 className={styles.title}>Primeiro projeto com NextJS</h1>
+    </div>
+    </>
+  )
+}
